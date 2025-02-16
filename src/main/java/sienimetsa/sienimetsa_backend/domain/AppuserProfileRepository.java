09b81b6403ebf00e70556d902204a87ec5b2513b
SophiaDun <sophia@sophia-MacBook-Air.local>
2025-02-16 14:17:39 +0200

    appprofilecontroller created

--- src/main/java/sienimetsa/sienimetsa_backend/domain/AppuserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package sienimetsa.sienimetsa_backend.domain;
-
-import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AppuserProfileRepository extends CrudRepository<AppuserProfile, Long> {
-    // Find profile by user
-    Optional<AppuserProfile> findByUser(Appuser user);
-}
