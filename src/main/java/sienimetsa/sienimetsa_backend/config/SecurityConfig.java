b1b5a39ed4f22529dfa67070c5cbe18b9bf74826
Maajoo <mio.kantola@hotmail.com>
2025-02-21 10:38:23 +0200

    add mushrooms endpoint

--- src/main/java/sienimetsa/sienimetsa_backend/config/SecurityConfig.java
+++ src/main/java/sienimetsa/sienimetsa_backend/config/SecurityConfig.java
@@ -44,8 +44,9 @@ public class SecurityConfig {
     @Order(1)
     public SecurityFilterChain mobileSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-            // Include /api/profile/** in addition to /mobile/**
-            .securityMatcher("/mobile/**", "/api/profile/**")
+            // ADD ENDPOINTS HERE TO USE WITH TOKEN
+            .securityMatcher("/mobile/**", "/api/profile/**", "/api/mushrooms/**")
+            
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
