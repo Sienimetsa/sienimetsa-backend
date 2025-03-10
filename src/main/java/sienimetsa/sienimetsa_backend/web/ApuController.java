99c0cf366485334ebfe995b4b20c2c77ef6a625c
Heta <hhmj28@gmail.com>
2025-03-10 12:11:07 +0200

    Update ApuController.java

--- src/main/java/sienimetsa/sienimetsa_backend/web/ApuController.java
+++ src/main/java/sienimetsa/sienimetsa_backend/web/ApuController.java
@@ -1,6 +1,8 @@
 package sienimetsa.sienimetsa_backend.web;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import sienimetsa.sienimetsa_backend.domain.Appuser;
 import sienimetsa.sienimetsa_backend.domain.AppuserRepository;
@@ -32,6 +35,24 @@ public class ApuController {
 
     @Autowired
     private UserRepository repository;
+
+    @GetMapping("/")
+    public Map<String, Object> getAllEndpoints() {
+        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().toUriString() + "/api";
+
+        Map<String, Object> response = new HashMap<>();
+        Map<String, Map<String, String>> links = new HashMap<>();
+
+        links.put("users", Map.of("href", baseUrl + "/users"));
+        links.put("mushrooms", Map.of("href", baseUrl + "/mushrooms"));
+        links.put("findings", Map.of("href", baseUrl + "/findings"));
+        links.put("appusers", Map.of("href", baseUrl + "/appusers"));
+        links.put("profile", Map.of("href", baseUrl + "/profile"));
+
+        response.put("_links", links);
+
+        return response;
+    }
 
     @GetMapping("/allusers")
     public List <User> getAllUsers() {
