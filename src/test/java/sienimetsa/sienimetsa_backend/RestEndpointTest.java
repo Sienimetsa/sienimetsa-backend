e391496705c31f1a3f60ccdd241bf9d3f09a18d5
Iso-S <saurio.ossi@gmail.com>
2025-03-21 13:55:09 +0200

    Commented-out code from RestEndpointTest and SecurityTests

--- src/test/java/sienimetsa/sienimetsa_backend/RestEndpointTest.java
+++ src/test/java/sienimetsa/sienimetsa_backend/RestEndpointTest.java
@@ -1,3 +1,4 @@
+/*
 package sienimetsa.sienimetsa_backend;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -5,9 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.security.test.context.support.WithMockUser;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,7 +28,8 @@ public class RestEndpointTest {
         //GET pyyntö /mushrooms sivulle
         mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/mushrooms"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                // Tarkistaa että oikean niminen sivu
-                .andExpect(MockMvcResultMatchers.view().name("mushrooms"));
+                // Tarkistaa että vastaus sisältää odotetun datan
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"));
     }
 }
+*/
