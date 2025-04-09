7d7692c08d0c3e9df6fc484cea0cff3933883cd2
Heta <hhmj28@gmail.com>
2025-04-09 20:46:09 +0300

    Optimized tests

--- src/test/java/sienimetsa/sienimetsa_backend/ControllerTest.java
+++ src/test/java/sienimetsa/sienimetsa_backend/ControllerTest.java
@@ -17,53 +17,44 @@ public class ControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private static final String[] WEB_CONTROLLER_ENDPOINTS = {
+        "/",
+        "/index",
+        "/frontpage",
+        "/users",
+        "/mushrooms",
+        "/findings"
+    };
+
+    private static final String[] APU_CONTROLLER_ENDPOINTS = {
+        "/apu/",
+        "/apu/allusers",
+        "/apu/allappusers",
+        "/apu/allmushrooms",
+        "/apu/allfindings",
+        "/apu/allprofiles"
+    };
+
     @Test
     void testWebControllerEndpoints() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/index"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/frontpage"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/users"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/mushrooms"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/findings"))
-                .andExpect(status().is3xxRedirection());
+        for (String endpoint : WEB_CONTROLLER_ENDPOINTS) {
+            mockMvc.perform(get(endpoint))
+                    .andExpect(status().is3xxRedirection());
+        }
     }
 
     @Test
     void testApuControllerEndpoints() throws Exception {
-        mockMvc.perform(get("/apu/"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/apu/allusers"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/apu/allappusers"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/apu/allmushrooms"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/apu/allfindings"))
-                .andExpect(status().is3xxRedirection());
-
-        mockMvc.perform(get("/apu/allprofiles"))
-                .andExpect(status().is3xxRedirection());
+        for (String endpoint : APU_CONTROLLER_ENDPOINTS) {
+                mockMvc.perform(get(endpoint))
+                        .andExpect(status().isOk());
+            }
     }
 
     @Test
     @WithMockUser(username = "testuser", roles = { "USER" })
     void testChatControllerWithMockUser() throws Exception {
         mockMvc.perform(get("/topic/publicChat"))
-                .andExpect(status().is4xxClientError()); // Odotetaan virhettä
+                .andExpect(status().is4xxClientError()); // Expecting client error as user is not authorized
     }
-
 }
